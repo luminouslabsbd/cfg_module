b1b3332054c133c581eeb6d9f2dd0e6b39e52e90
Ahshanul Haider Tanha <61645636+AhshanulHaiderTAnha@users.noreply.github.com>
2024-07-16 21:24:14 +0600

    code update

---
+++ config/my_config.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+// $config['csrf_token_name']   = defined('APP_CSRF_TOKEN_NAME') ? APP_CSRF_TOKEN_NAME : 'csrf_token_name';
+
+$config['csrf_exclude_uris_cfg_module'] = ['forms/wtl/[0-9a-z]+', 'forms/ticket', 'forms/quote/[0-9a-z]+', 'admin/tasks/timer_tracking', 'api\/.+', 'razorpay/success\/.+',
+'admin/cfg_module/cfg_module/custom_fields_group','admin/cfg_module/cfg_module/delete_group','cfg_module/cfg_module/update_fields_group'];
