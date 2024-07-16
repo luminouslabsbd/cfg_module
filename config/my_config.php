63e7307d0ea74631fab5aa20494c1cbc89fb6037
Ahshanul Haider Tanha <61645636+AhshanulHaiderTAnha@users.noreply.github.com>
2024-07-16 21:34:40 +0600

    code update

--- config/my_config.php
+++ config/my_config.php
@@ -3,5 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // $config['csrf_token_name']   = defined('APP_CSRF_TOKEN_NAME') ? APP_CSRF_TOKEN_NAME : 'csrf_token_name';
 
-$config['csrf_exclude_uris_cfg_module'] = ['forms/wtl/[0-9a-z]+', 'forms/ticket', 'forms/quote/[0-9a-z]+', 'admin/tasks/timer_tracking', 'api\/.+', 'razorpay/success\/.+',
-'admin/cfg_module/cfg_module/custom_fields_group','admin/cfg_module/cfg_module/delete_group','cfg_module/cfg_module/update_fields_group'];
+$config['csrf_exclude_uris_cfg_module'] = ['forms/wtl/[0-9a-z]+', 'forms/ticket', 'forms/quote/[0-9a-z]+', 'admin/tasks/timer_tracking', 'api\/.+', 
+'razorpay/success\/.+','admin/cfg_module/cfg_module/custom_fields_group','admin/cfg_module/cfg_module/delete_group',
+'cfg_module/cfg_module/update_fields_group','admin/clients/load_custom_fields','admin/clients/custom_field_order_update'];
